174acc4ddddd5769843a4e3437b9d33f9ec8ac34
Ali <alice.krouse@gmail.com>
2019-01-23 17:25:08 -0600

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class PlayerController : MonoBehaviour
+{
+    private Player p;
+    private Rigidbody rb;
+    private AudioSource source;
+
+    public float maxSpeed, acceleration, turnSpeed;
+    private float speed;
+
+    public AudioClip carStart, carDrive, carStop;
+    
+	void Start ()
+    {
+        p = ReInput.players.GetPlayer(0);
+        rb = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
+	}
+	
+	void Update ()
+    {
+        speed = Mathf.Clamp(p.GetAxis("drive") * (Time.deltaTime * acceleration), -maxSpeed, maxSpeed);
+        transform.Translate(Vector3.forward * speed);
+        //Vector3 worldVelocity = new Vector3(rb.velocity.x, rb.velocity.y, speed);
+        //rb.velocity = transform.TransformDirection(worldVelocity);
+        //rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, speed);
+
+        float rotation = p.GetAxis("turn") * turnSpeed;
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + rotation, 0);
+    }
+}
