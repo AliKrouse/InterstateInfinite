203a0a413d53cadb6764c18bc0e4169a39ed19ca
Ali <alice.krouse@gmail.com>
2019-01-24 14:19:10 -0600

    added player vehicle randomization, road creation, and sky

---
+++ Assets/Scripts/CreatePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePlayer : MonoBehaviour
+{
+    public GameObject[] vehicles;
+    public Material[] carColors;
+    public Material[] truckColors;
+    public Material[] suvColors;
+    
+	void Start ()
+    {
+        int choice = Random.Range(0, vehicles.Length);
+        GameObject player = Instantiate(vehicles[choice]);
+        if (choice == 0)
+            player.transform.GetChild(0).GetComponent<MeshRenderer>().material = carColors[Random.Range(0, carColors.Length)];
+        if (choice == 1)
+            player.transform.GetChild(0).GetComponent<MeshRenderer>().material = truckColors[Random.Range(0, truckColors.Length)];
+        if (choice == 2)
+            player.transform.GetChild(0).GetComponent<MeshRenderer>().material = suvColors[Random.Range(0, truckColors.Length)];
+        transform.parent = player.transform;
+        Destroy(this);
+	}
+}
---
+++ Assets/Scripts/CreateRoad.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRoad : MonoBehaviour
+{
+    public GameObject[] availableRoads;
+    public GameObject[] availableStops;
+
+    public GameObject nextSegment;
+    private Transform spawnPoint;
+    public int DCToSpawnStop;
+    public float distanceToSpawnNextRoad;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        if (Vector3.Distance(transform.position, nextSegment.transform.position) < distanceToSpawnNextRoad)
+            Create();
+	}
+
+    void Create()
+    {
+        int roll = Random.Range(1, 101);
+        spawnPoint = nextSegment.transform.GetChild(0).GetChild(1);
+        if (roll < DCToSpawnStop)
+        {
+            nextSegment = Instantiate(availableRoads[Random.Range(0, availableRoads.Length)], spawnPoint.position, spawnPoint.rotation);
+        }
+        else
+        {
+            nextSegment = Instantiate(availableStops[Random.Range(0, availableStops.Length)], spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+}
